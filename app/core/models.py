914c0103c2ee0917ee2440eed98baae939c9b25f
Conor <conor.fahy@mfn.berlin>
2022-03-24 16:34:38 +0100

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 # Create your models here.
@@ -61,5 +60,23 @@ class Ingredient(models.Model):
     )
 
     def __str__(self):
-        return self.name          
+        return self.name 
 
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+
+    def __str__(self):
+        return self.title    
+           
+           
