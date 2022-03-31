654d7d8f808451abc0118febf3abb0a83a519994
Conor <conor.fahy@mfn.berlin>
2022-03-31 17:36:49 +0200

    added image field to recipe model

--- app/app/settings.py
+++ app/app/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
     'user',
-    'recipe',
+    'recipe'
 ]
 
 MIDDLEWARE = [
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = 'vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
